61936d1f2ab49fd22eb5002f5e22afc46779fc55
박진영 <92846785+jyp5555@users.noreply.github.com>
2025-12-15 22:48:20 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle < 90){
+            return 1;
+        }else if (angle % 90 == 0){
+            return angle / 90 * 2;
+        }else{
+            return 3 ;
+        }
+    }
+}
